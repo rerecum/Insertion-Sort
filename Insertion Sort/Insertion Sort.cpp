569ed3aa6e562dfb68c05a6e4c0b68c11bece1d4
rerecum <kacperkacperkacper132@gmail.com>
2021-12-31 01:09:29 +0100

    insertionsort function

--- Insertion Sort/Insertion Sort.cpp
+++ Insertion Sort/Insertion Sort.cpp
@@ -6,7 +6,19 @@ using namespace std;
 
 void insertionSort(int arr[])
 {
+    int j = 0;
+    int key = 0;
 
+    for (int i = 1; i < 5; i++)
+    {
+        key = arr[i]; // outer loop
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) // inner loop
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+    }
 }
 
 int main()
@@ -26,7 +38,7 @@ int main()
 
     insertionSort(myarray); 
 
-    cout << "Po sortowaniu: " << endl;
+    cout << endl << "Po sortowaniu: " << endl;
     for (int i = 0; i < 5; i++)
     {
         cout << myarray[i] << " ";
